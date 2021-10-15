28a04930d8bb59bafbd7571cb161863cd028072c
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2021-10-15 11:52:08 -0500

    chose: Version 1.5.17

--- src/components/livestreaming/index.jsx
+++ src/components/livestreaming/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Livestreaming } from '@jcgalvis/vtex.livestreaming';
-import '@jcgalvis/vtex.livestreaming/dist/index.css';
+import React from "react";
+import { Livestreaming } from "@jcgalvis/vtex.livestreaming";
+import "@jcgalvis/vtex.livestreaming/dist/index.css";
 
 const LivestreamingPortal = () => {
-    return (
-        <Livestreaming idLivestreaming="__IDLIVESTREAMING" account="__ACCOUNT" />
-    );
+  return (
+    <Livestreaming
+      idLivestreaming="__IDLIVESTREAMING"
+      account="__ACCOUNT"
+      inactiveSidebarProducts="_INACTIVESIDEBARPRODUCTS"
+      inactiveProductsCarousel="_INACTIVEPRODUCTSCAROUSEL"
+      inactivateChat="_INACTIVATECHAT"
+      inactivateLike="_INACTIVATELIKE"
+      inactivateViewers="_INACTIVATEVIEWERS"
+      isInfinite="_ISINFINITE"
+      time="_TIME"
+      pdp="_PDP"
+      originOfProducts="_ORIGINOFPRODUCTS"
+      kuikpay="_KUIKPAY"
+    />
+  );
 };
 
 export default LivestreamingPortal;
