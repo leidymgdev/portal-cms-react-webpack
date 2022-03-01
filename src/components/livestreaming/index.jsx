541b7c9d304e3506a7a7fed148e6847900516f84
JCGALVIS <jcgalvis6@hotmail.com>
2022-03-01 10:49:53 -0500

    chose: Version 1.6.76-beta

--- src/components/livestreaming/index.jsx
+++ src/components/livestreaming/index.jsx
@@ -82,6 +82,7 @@ const LivestreamingPortal = () => {
     <Livestreaming
       addToCart={addToCart}
       account="__ACCOUNT"
+      environment="dev"
       getProductId={getProductByIdCace}
       getProducts={getProductsCace}
       idLivestreaming="__IDLIVESTREAMING"
