867b3e431f2742ae2b5edfe814bae2abfc8bd8f7
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2022-04-11 17:24:28 -0500

    Version vtex.livestreaming: 1.8.0 New production version for Sprint

--- src/components/livestreaming/index.jsx
+++ src/components/livestreaming/index.jsx
@@ -78,7 +78,7 @@ const LivestreamingPortal = () => {
     <Livestreaming
       addToCart={addToCart}
       account="__ACCOUNT"
-      environment="dev"
+      environment="prod"
       getProductId={getProductByIdCace}
       getProducts={getProductsCace}
       idLivestreaming="__IDLIVESTREAMING"
